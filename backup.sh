29433a69079ffc8b50bc70f4d620e8f30d6409ff
Sadeq Talaq <165420981+smhatc@users.noreply.github.com>
2025-09-12 01:05:43 +0300

    Added optional exclude.txt file usage and slightly modified script output to only display what the user needs to know

--- backup.sh
+++ backup.sh
@@ -19,7 +19,6 @@ mapfile -t possible_mountpoints < <(grep -v '^\s*#' "$mount_points_file" | grep 
 
 # Check which mount points are mounted and set drive_path. If multiple are mounted the user is asked to choose
 mounted=()
-drive_path=""
 for path in "${possible_mountpoints[@]}"; do
     [[ -d "$path" ]] && mounted+=("$path")
 done
@@ -28,23 +27,19 @@ if [[ ${#mounted[@]} -eq 0 ]]; then
     echo "${error_icon} None of the configured mount points are mounted."
     exit
 elif [[ ${#mounted[@]} -eq 1 ]]; then
-    echo "${process_icon} Setting the mounted drive to use for the backup destination..."
     drive_path="${mounted[0]}"
-    echo "${success_icon} Using mounted drive at \"${drive_path}\" as the backup destination."
 else
     echo "${process_icon} Multiple mounted drives found. Please choose one to use for the backup destination:"
     select choice in "${mounted[@]}"; do
         if [[ -n "$choice" ]]; then
             drive_path="$choice"
-            echo "${success_icon} Using mounted drive at \"${drive_path}\" as the backup destination."
+            echo "${success_icon} Using mounted drive at \"${drive_path}\" as the backup destination." && echo "$line_separator"
             break
         else
             echo "${error_icon} Invalid selection. Try again."
         fi
     done
 fi
-
-echo "$line_separator"
 
 # Backup settings
 current_date=$(date +%Y-%m-%d_%H-%M-%S) # Current date in a specific format for the archiving functionality
@@ -70,6 +65,22 @@ if [[ ! -d "$file_backup_arc" ]]; then
     mkdir -p "$file_backup_arc" && echo "${success_icon} Backup archive directory has been created." && echo "$line_separator"
 fi
 
+# Allow for optional --exclude-from using exclude.txt file
+exclude_file="./exclude.txt"
+
+if [[ -f "$exclude_file" ]]; then
+    echo "${process_icon} exclude.txt file detected. Applying exclude list..."
+    exclude_opt="--exclude-from=${exclude_file}"
+    echo "${success_icon} Applied exclude list successfully." && echo "$line_separator"
+else
+    exclude_opt=""
+fi
+
 # Perform backup
 echo "${process_icon} Starting backup..."
-rsync -avhPb --backup-dir "${file_backup_arc}/${current_date}" --delete "${file_backup_src}" "${file_backup_dir}" && echo "${success_icon} Backup completed successfully on ${current_date}."
+rsync -avhPb "${exclude_opt}" \
+    --backup-dir "${file_backup_arc}/${current_date}" \
+    --delete \
+    "${file_backup_src}" \
+    "${file_backup_dir}" && \
+    echo "${success_icon} Backup completed successfully on ${current_date}."
